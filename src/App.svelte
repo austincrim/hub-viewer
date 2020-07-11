749396790c5e0f9437a6e0b15c8d0374a2e57f5a
Austin <aust.crim@gmail.com>
2020-07-11 17:19:49 -0500

    fetch repos

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,55 @@
 <script>
-	import TailwindStyles from './TailwindStyles.svelte';
+    import TailwindStyles from "./TailwindStyles.svelte";
+    import Repo from "./components/Repo.svelte";
+
+    let ghUsername = "";
+    let repos = [];
+
+    async function getReposByUser() {
+        const res = await fetch(
+            `https://api.github.com/users/${ghUsername}/repos`
+        );
+        const data = await res.json();
+        repos = data;
+    }
+
+    window.onload = function() {
+        document.querySelector('#gh-username').addEventListener("keyup", (e) => {
+            if (e.keyCode === 13) {
+                e.preventDefault();
+                document.getElementById("getReposBtn").click();
+            }
+        });
+    }
 </script>
 
+<div class="flex flex-col bg-gray-100 min-h-screen py-12">
+    <div class="flex justify-center space-x-4 py-4">
+        <input
+            id="gh-username"
+            placeholder="Github Username"
+            bind:value={ghUsername}
+            class="px-3 w-1/3 rounded-lg shadow-lg"
+            type="text" />
 
+        <button
+            id="getReposBtn"
+            on:click={getReposByUser}
+            class="px-4 py-2 text-gray-100 bg-purple-600 rounded
+            hover:bg-purple-700">
+            Get Repos
+        </button>
+    </div>
 
-<div class='bg-teal-300'>
-	Hello World
+    <div class="flex flex-col justify-center items-center">
+        {#await repos}
+            <p>loading repos...</p>
+        {:then repos}
+            {#each repos as repo}
+                <div class="w-1/2">
+                    <Repo {repo} />
+                </div>
+            {/each}
+        {/await}
+    </div>
 </div>
---
+++ src/components/Repo.svelte
@@ -0,0 +1,87 @@
+<script>
+    export let repo;
+</script>
+
+<style>
+    .repo {
+        transition: transform .2s ease-out, box-shadow .2s ease-out;
+    }
+    .repo:hover {
+        transform: translateY(-2px);
+    }
+</style>
+
+
+<div class="flex items-center justify-between p-4 mt-4 bg-gray-200 rounded shadow-md hover:shadow-lg repo">
+    <span class="text-xl">
+        <a
+            href={repo.html_url}
+            class="hover:text-purple-600 transition-colors duration-75 ease-in">
+            {repo.name}
+        </a>
+    </span>
+    <div class="flex space-x-4">
+        <div class="flex flex-col items-center">
+            <svg
+                class="text-gray-800 w-6 h-6"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1
+                    0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1
+                    0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538
+                    1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976
+                    2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0
+                    00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1
+                    1 0 00.951-.69l1.519-4.674z" />
+            </svg>
+            <div class='text-lg'>
+                {repo.stargazers_count}
+            </div>
+        </div>
+        <div class="flex flex-col items-center">
+            <svg
+                class="text-gray-800 w-6 h-6"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943
+                    9.542 7-1.274 4.057-5.064 7-9.542 7-4.477
+                    0-8.268-2.943-9.542-7z" />
+            </svg>
+            <div class='text-lg'>
+                {repo.watchers_count}
+            </div>
+        </div>
+        <div class="flex flex-col items-center">
+            <svg
+                class='text-gray-800 w-6 h-6'
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                    d="M8.684 13.342C8.886 12.938 9 12.482 9
+                    12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0
+                    2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0
+                    105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368
+                    2.684 3 3 0 00-5.368-2.684z" />
+            </svg>
+            <div class='text-lg'>
+                {repo.forks}
+            </div>
+        </div>
+    </div>
+
+</div>
